c3d8496f7ea2aae36833d48bf4cac5497ed49f29
Paul133 <paulbalint2000@yahoo.com>
2020-11-29 17:07:40 +0000

    ADD: Another approach for the Timer class

---
+++ Questionnaire Framework/TimerV2.cpp
@@ -0,0 +1,23 @@
+#include "TimerV2.h"
+
+void TimerV2::cronometru(int ore, int minute, int secunde)
+{
+	for (;;)
+	{
+		if (ore == 0 && minute == 0 && secunde == 0)
+		{
+			break;
+		}
+		if (secunde == 0 && minute == 0)
+		{
+			minute = 60;
+			ore--;
+		}
+		if (secunde == 0)
+		{
+			secunde = 60;
+			minute--;
+		}
+		cout << ore << ":" << minute << ":" << secunde--;
+	}
+}
---
+++ Questionnaire Framework/TimerV2.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+using namespace std;
+
+class TimerV2
+{
+public:
+	void cronometru(int ore, int minute, int secunde);
+
+};
+
