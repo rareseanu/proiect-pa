d609ed0a57738b5ce3836b569b2f9d281e9f6f48
Paul133 <paulbalint2000@yahoo.com>
2021-01-29 20:17:41 +0000

    UPDATE:Trying a new approach

--- Questionnaire Framework/Handler.v2.h
+++ Questionnaire Framework/Handler.v2.h
@@ -1,106 +1,32 @@
-#pragma once
+#include <fstream>
 #include <iostream>
-#include <windows.h>
-#include <sqlext.h>
-#include <sqltypes.h>
-#include <sql.h>
 
 using namespace std;
 
+#import C:\\Program Files\\Common Files\\system\\ado\\msado15.dll rename("EOF",
+"AdoNSEOF")
+
+_bstr_t bstrConnect = "Provider=Microsoft.ACE.OLEDB.12.0;Data "
+"Source=C:\\Users\\lriley\\Documents\\Northwind 2007.mdb;";
+
+HRESULT hr;
+
 int main()
 {
+    ::CoInitialize(NULL);
+    const char* DAM = "ADO";
 
-#define SQL_RESULT_LEN 240
-#define SQL_RETURN_CODE_LEN 1000
-
-    SQLHANDLE sqlConnHandle;
-    SQLHANDLE sqlStmtHandle;
-    SQLHANDLE sqlEnvHandle;
-    SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
-
-    sqlConnHandle = NULL;
-    sqlStmtHandle = NULL;
-
-    if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle))
+    ADODB::_ConnectionPtr pConn("ADODB.Connection");
+    hr = pConn->Open(bstrConnect, "admin", "", ADODB::adConnectUnspecified);
+    if (SUCCEEDED(hr))
     {
-        goto COMPLETED;
+        cout << DAM << ": Successfully connected to database. Data source name:\n  "
+            << pConn->GetConnectionString() << endl;
     }
-
-    if (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
+    else
     {
-        goto COMPLETED;
+        cout << DAM << " : Unable to connect to data source: " << bstrConnect << endl;
     }
-
-    if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle))
-    {
-        goto COMPLETED;
-    }
-
-    cout << "Attempting connection to SQL Server...";
-    cout << "\n";
-
-    switch (SQLDriverConnect(sqlConnHandle,
-        NULL,
-        (SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 1433;DATABASE=master;Trusted=true;",
-        SQL_NTS,
-        retconstring,
-        1024,
-        NULL,
-        SQL_DRIVER_NOPROMPT)) {
-    case SQL_SUCCESS:
-        cout << "Successfully connected to SQL Server";
-        cout << "\n";
-        break;
-    case SQL_SUCCESS_WITH_INFO:
-        cout << "Successfully connected to SQL Server";
-        cout << "\n";
-        break;
-    case SQL_INVALID_HANDLE:
-        cout << "Could not connect to SQL Server";
-        cout << "\n";
-        goto COMPLETED;
-    case SQL_ERROR:
-        cout << "Could not connect to SQL Server";
-        cout << "\n";
-        goto COMPLETED;
-
-    default:
-        break;
-    }
-
-    if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
-        goto COMPLETED;
-
-    cout << "\n";
-    cout << "Executing T-SQL query...";
-    cout << "\n";
-
-    if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)L"SELECT @@VERSION", SQL_NTS))
-    {
-        cout << "Error querying SQL Server";
-        cout << "\n";
-        goto COMPLETED;
-    }
-    else 
-    {
-        SQLCHAR sqlVersion[SQL_RESULT_LEN];
-        SQLINTEGER ptrSqlVersion;
-
-        while (SQLFetch(sqlStmtHandle) == SQL_SUCCESS)
-        {
-            SQLGetData(sqlStmtHandle, 1, SQL_CHAR, sqlVersion, SQL_RESULT_LEN, &ptrSqlVersion);
-
-            cout << "\nQuery Result:\n\n";
-            cout << sqlVersion << endl;
-        }
-    }
-
-COMPLETED:
-    SQLFreeHandle(SQL_HANDLE_STMT, sqlStmtHandle);
-    SQLDisconnect(sqlConnHandle);
-    SQLFreeHandle(SQL_HANDLE_DBC, sqlConnHandle);
-    SQLFreeHandle(SQL_HANDLE_ENV, sqlEnvHandle);
-
-    cout << "\nPress any key to exit...";
-    getchar();
-}   
+    ::CoUninitialize();
+    return 0;
+}
