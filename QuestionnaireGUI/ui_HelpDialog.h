3511e16ca9199ad0f0786646624ecf514151b7ef
Alin Blendea <alinblendea1708@gmail.com>
2021-01-30 12:09:32 +0200

    UPDATE: Removed the calculator from the help window.

--- QuestionnaireGUI/ui_HelpDialog.h
+++ QuestionnaireGUI/ui_HelpDialog.h
@@ -20,8 +20,6 @@ QT_BEGIN_NAMESPACE
 class Ui_HelpDialog
 {
 public:
-    QLabel *calculatorIconLabel;
-    QLabel *calculatorInfoLabel;
     QLabel *previousIconLabel;
     QLabel *clearIconLabel;
     QLabel *nextIconLabel;
@@ -42,47 +40,38 @@ public:
         QIcon icon;
         icon.addFile(QString::fromUtf8("images/help.png"), QSize(), QIcon::Normal, QIcon::Off);
         HelpDialog->setWindowIcon(icon);
-        calculatorIconLabel = new QLabel(HelpDialog);
-        calculatorIconLabel->setObjectName(QString::fromUtf8("calculatorIconLabel"));
-        calculatorIconLabel->setGeometry(QRect(170, 120, 41, 39));
-        calculatorIconLabel->setMinimumSize(QSize(20, 39));
-        calculatorIconLabel->setPixmap(QPixmap(QString::fromUtf8("images/calculator.png")));
-        calculatorIconLabel->setScaledContents(true);
-        calculatorInfoLabel = new QLabel(HelpDialog);
-        calculatorInfoLabel->setObjectName(QString::fromUtf8("calculatorInfoLabel"));
-        calculatorInfoLabel->setGeometry(QRect(220, 130, 211, 21));
         previousIconLabel = new QLabel(HelpDialog);
         previousIconLabel->setObjectName(QString::fromUtf8("previousIconLabel"));
-        previousIconLabel->setGeometry(QRect(170, 180, 41, 41));
+        previousIconLabel->setGeometry(QRect(180, 140, 41, 41));
         previousIconLabel->setPixmap(QPixmap(QString::fromUtf8("images/backButton.png")));
         previousIconLabel->setScaledContents(true);
         clearIconLabel = new QLabel(HelpDialog);
         clearIconLabel->setObjectName(QString::fromUtf8("clearIconLabel"));
-        clearIconLabel->setGeometry(QRect(170, 240, 41, 41));
+        clearIconLabel->setGeometry(QRect(180, 200, 41, 41));
         clearIconLabel->setPixmap(QPixmap(QString::fromUtf8("images/clearButton.png")));
         clearIconLabel->setScaledContents(true);
         nextIconLabel = new QLabel(HelpDialog);
         nextIconLabel->setObjectName(QString::fromUtf8("nextIconLabel"));
-        nextIconLabel->setGeometry(QRect(170, 300, 41, 41));
+        nextIconLabel->setGeometry(QRect(180, 260, 41, 41));
         nextIconLabel->setPixmap(QPixmap(QString::fromUtf8("images/nextButton.png")));
         nextIconLabel->setScaledContents(true);
         finishIconLabel = new QLabel(HelpDialog);
         finishIconLabel->setObjectName(QString::fromUtf8("finishIconLabel"));
-        finishIconLabel->setGeometry(QRect(170, 360, 41, 41));
+        finishIconLabel->setGeometry(QRect(180, 320, 41, 41));
         finishIconLabel->setPixmap(QPixmap(QString::fromUtf8("images/finishButton.png")));
         finishIconLabel->setScaledContents(true);
         previousInfoLabel = new QLabel(HelpDialog);
         previousInfoLabel->setObjectName(QString::fromUtf8("previousInfoLabel"));
-        previousInfoLabel->setGeometry(QRect(220, 190, 161, 21));
+        previousInfoLabel->setGeometry(QRect(230, 150, 161, 21));
         clearInfoLabel = new QLabel(HelpDialog);
         clearInfoLabel->setObjectName(QString::fromUtf8("clearInfoLabel"));
-        clearInfoLabel->setGeometry(QRect(220, 250, 111, 21));
+        clearInfoLabel->setGeometry(QRect(230, 210, 111, 21));
         nextInfoLabel = new QLabel(HelpDialog);
         nextInfoLabel->setObjectName(QString::fromUtf8("nextInfoLabel"));
-        nextInfoLabel->setGeometry(QRect(220, 310, 121, 21));
+        nextInfoLabel->setGeometry(QRect(230, 270, 121, 21));
         finishInfoLabel = new QLabel(HelpDialog);
         finishInfoLabel->setObjectName(QString::fromUtf8("finishInfoLabel"));
-        finishInfoLabel->setGeometry(QRect(220, 370, 101, 21));
+        finishInfoLabel->setGeometry(QRect(230, 330, 101, 21));
         buttonsLabel = new QLabel(HelpDialog);
         buttonsLabel->setObjectName(QString::fromUtf8("buttonsLabel"));
         buttonsLabel->setGeometry(QRect(130, 60, 301, 31));
@@ -95,8 +84,6 @@ public:
     void retranslateUi(QDialog *HelpDialog)
     {
         HelpDialog->setWindowTitle(QCoreApplication::translate("HelpDialog", "Help", nullptr));
-        calculatorIconLabel->setText(QString());
-        calculatorInfoLabel->setText(QCoreApplication::translate("HelpDialog", "<html><head/><body><p><span style=\" font-size:10pt;\">- launches a calculator.</span></p></body></html>", nullptr));
         previousIconLabel->setText(QString());
         clearIconLabel->setText(QString());
         nextIconLabel->setText(QString());
